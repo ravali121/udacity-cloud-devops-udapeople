7ed58d22fa645a77544cde433dd90f088cd08314
Ravali Peddi <ravali@philo.com>
2022-08-21 16:36:46 -0700

    add test phases to the pipeline

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -28,7 +28,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .addTag('customTag')
     .setBasePath(apiVersionPrefix)
-    .addBearerAuth()
+    .addBearerAuth();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup(`api/${apiVersionPrefix}`, app, document);
   const config: ConfigService = app.get('ConfigService');
